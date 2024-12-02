423c121eaaf3fe5cf7da365a42d983f3f4e99a1b
Laura Fumagalli <laura.rodrigues@zup.com.br>
2024-12-02 17:07:35 -0300

    feat: add today's exercises

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero = 10; // Inteiro (Tipo primitivo)
+        double numeroDecimal = 10.55; // Número decimal (Tipo primitivo)
+        long numeroLongo = 12345678912l; // Número longo (tipo primitivo)
+        float numeroFlutuante = 12.123456f; // Número flutuante (tipo primitivo)
+
+        boolean boleano = true; // Boleano (tipo primitivo) - true ou false
+        char caracter = 'M'; // Caracteres individuais (tipo primitivo)
+
+        String frase = "Catalisa é muito legal"; // Cadeia de caracteres (tipo de referencia)
+
+        int numeroConvertido = (int)11.00; // conversão casting
+        int novoNumeroFlutuante = 11;
     }
 }
